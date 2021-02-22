8490d42cafb962ed9cf85e96eced390ed012d7d4
jwei8 <skybeach221b@gmail.com>
2021-02-21 22:25:04 -0800

    first commit

---
+++ expense-tracker/client/src/context/AppReducer.js
@@ -0,0 +1,30 @@
+//respond to application state changes and update the program
+
+export default (state, action) => {
+    switch (action.type) {
+        case 'GET_TRANSACTIONS':
+            return {
+                ...state,
+                loading: false,
+                transactions: action.payload
+            }
+        case 'DELETE_TRANSACTION':
+            return {
+                ...state,
+                transactions: state.transactions.filter(transaction => transaction._id !== action.payload)
+            }
+        case 'ADD_TRANSACTION':
+            return {
+                ...state,
+                transactions: [...state.transactions, action.payload]//get the original transactions
+            }
+        case 'TRANSACTION_ERROR':
+            return {
+                ...state,
+                error: action.payload
+            }
+        default:
+            return state;
+    }
+
+}
